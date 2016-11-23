9ba0b3d09147aadc0dd6271309513f714d7004e3
curtisnoel <curtis.noel@gmail.com>
2016-11-22 20:24:28 -0500

    added feature scaling, refactored into template

--- preprocessing/data_preprocessing_template.R
+++ preprocessing/data_preprocessing_template.R
@@ -2,30 +2,36 @@
 
 # Importing the dataset
 dataset = read.csv('Data.csv')
-
-
-#fill in missing data using column mean
-dataset$Age=ifelse(is.na(dataset$Age),
-                   ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
-                   dataset$Age)
-
-dataset$Salary=ifelse(is.na(dataset$Salary),
-                      ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
-                      dataset$Salary)
-
-#encode categories
-dataset$Country = factor(dataset$Country,
-                         levels = c('France', 'Spain', 'Germany'),
-                         labels = c(1, 2, 3))
-dataset$Purchased = factor(dataset$Purchased,
-                         levels = c('No', 'Yes'),
-                         labels = c(0, 1))
+#dataset = dataset[, 2:3]
 
 #split dataset and test set
-install.packages('caTools')
+#install.packages('caTools')
 library('caTools')
 set.seed(123)
 split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
+
+
+# #fill in missing data using column mean
+# dataset$Age=ifelse(is.na(dataset$Age),
+#                    ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+#                    dataset$Age)
+# 
+# dataset$Salary=ifelse(is.na(dataset$Salary),
+#                       ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+#                       dataset$Salary)
+
+# #encode categories
+# dataset$Country = factor(dataset$Country,
+#                          levels = c('France', 'Spain', 'Germany'),
+#                          labels = c(1, 2, 3))
+# dataset$Purchased = factor(dataset$Purchased,
+#                          levels = c('No', 'Yes'),
+#                          labels = c(0, 1))
+
+
+# #scale
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
 
